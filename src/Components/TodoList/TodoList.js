df467ca5013bb062b344b31c4b2c0b4f98fb5aa0
Amr <amr.webdevelopment@gmail.com>
2021-11-12 21:54:30 +0200

    modal & create form

--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, {useState} from 'react';
 import styles from './TodoList.module.scss';
+import CreateTask from '../../Modals/CreateTask/CreateTask'
 
-const TodoList = () => (
-  <div className={styles.TodoList}>
-    TodoList Component
-  </div>
-);
 
-TodoList.propTypes = {};
-
-TodoList.defaultProps = {};
+const TodoList = () => {
+  const [modal, setModal] = useState(false);
+  const toggle = () => {
+          setModal(!modal);
+      }
+  return (
+    <>
+    <div className = {"text-center",styles.header}>
+      <h3>Todo List</h3>
+      <button className = "btn btn-primary mt-2" onClick = {() => setModal(true)}>Create Task</button>
+    </div>
+    <div className = "task-container">
+    </div>
+    <CreateTask toggle = {toggle} modal = {modal} />
+  </>
+  )
+  
+}
 
 export default TodoList;
--- src/Modals/CreateTask/CreateTask.js
+++ src/Modals/CreateTask/CreateTask.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './CreateTask.module.scss';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-const CreateTask = () => (
-  <div className={styles.CreateTask}>
-    CreateTask Component
-  </div>
+const CreateTask = ({modal, toggle}) => (
+ 
+        <Modal isOpen={modal} toggle={toggle}>
+            <ModalHeader toggle={toggle}>Create Task</ModalHeader>
+            <ModalBody>
+            <div className = "form-group">
+                        <label>Task Name</label>
+                        <input type="text" className = "form-control" name = "taskName"/>
+                    </div>
+                    <div className = "form-group">
+                        <label>Description</label>
+                        <textarea rows = "5" className = "form-control"  name = "description"></textarea>
+                    </div>
+                
+            </ModalBody>
+            <ModalFooter>
+            <Button color="primary" >Create</Button>{' '}
+            <Button color="secondary" onClick={toggle}>Cancel</Button>
+            </ModalFooter>
+      </Modal>
+   
 );
 
 CreateTask.propTypes = {};
