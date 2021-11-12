4ac0058c422ca1981ab35807870fe85af6536ef7
Amr <amr.webdevelopment@gmail.com>
2021-11-12 22:28:23 +0200

    create & save task localstorage

--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -5,9 +5,17 @@ import CreateTask from '../../Modals/CreateTask/CreateTask'
 
 const TodoList = () => {
   const [modal, setModal] = useState(false);
+  const [taskList, setTaskList] = useState([])
   const toggle = () => {
           setModal(!modal);
       }
+      const saveTask = (taskObj) => {
+        let tempList = taskList
+        tempList.push(taskObj)
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(taskList)
+        setModal(false)
+    }
   return (
     <>
     <div className = {"text-center",styles.header}>
@@ -15,8 +23,9 @@ const TodoList = () => {
       <button className = "btn btn-primary mt-2" onClick = {() => setModal(true)}>Create Task</button>
     </div>
     <div className = "task-container">
+      {taskList && taskList.map((obj) =>  <p>{obj.Name}</p> )}
     </div>
-    <CreateTask toggle = {toggle} modal = {modal} />
+    <CreateTask toggle = {toggle} modal = {modal} save = {saveTask}/>
   </>
   )
   
--- src/Modals/CreateTask/CreateTask.js
+++ src/Modals/CreateTask/CreateTask.js
@@ -1,30 +1,53 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import styles from './CreateTask.module.scss';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-const CreateTask = ({modal, toggle}) => (
- 
+const CreateTask = ({modal, toggle, save}) => {
+ const [taskName, setTaskName] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleChange = (e) => {
+        
+        const {name, value} = e.target
+
+        if(name === "taskName"){
+            setTaskName(value)
+        }else{
+            setDescription(value)
+        }
+
+
+    }
+
+    const handleSave = (e) => {
+        e.preventDefault()
+        let taskObj = {}
+        taskObj["Name"] = taskName
+        taskObj["Description"] = description
+        save(taskObj)
+
+    }
+    return (
         <Modal isOpen={modal} toggle={toggle}>
             <ModalHeader toggle={toggle}>Create Task</ModalHeader>
             <ModalBody>
             <div className = "form-group">
                         <label>Task Name</label>
-                        <input type="text" className = "form-control" name = "taskName"/>
+                        <input type="text" className = "form-control" value = {taskName} onChange = {handleChange} name = "taskName"/>
                     </div>
                     <div className = "form-group">
                         <label>Description</label>
-                        <textarea rows = "5" className = "form-control"  name = "description"></textarea>
-                    </div>
-                
+                        <textarea rows = "5" className = "form-control" value = {description} onChange = {handleChange} name = "description"></textarea>
+              </div>
             </ModalBody>
             <ModalFooter>
-            <Button color="primary" >Create</Button>{' '}
+            <Button color="primary" onClick={handleSave}>Create</Button>{' '}
             <Button color="secondary" onClick={toggle}>Cancel</Button>
             </ModalFooter>
       </Modal>
+      )
    
-);
+};
 
 CreateTask.propTypes = {};
 
